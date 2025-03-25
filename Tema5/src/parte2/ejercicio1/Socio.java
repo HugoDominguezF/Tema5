84161c458d55b5de182cb86c3c0654abb8d0f9c2
HugoDominguezF <hugo.dominguez@iesnervion.es>
2025-03-25 13:08:00 +0100

    ejercicios

---
+++ Tema5/src/parte2/ejercicio1/Socio.java
@@ -0,0 +1,32 @@
+package parte2.ejercicio1;
+
+public class Socio implements Comparable<Socio> {
+
+	private int id;
+	private int edad;
+	private String nombre;
+
+	public Socio(int id, int edad, String nombre) {
+
+		this.id = id;
+		this.edad = edad;
+		this.nombre = nombre;
+	}
+
+	public String toString() {
+		return "Id: " + id + ", Nombre: " + nombre + ", Edad: " + edad;
+	}
+
+	public int compareTo(Socio a) {
+
+		int res = 0;
+		Socio nuevo = (Socio) a;
+		if (this.id < nuevo.id) {
+			res = -1;
+		} else if (this.id > nuevo.id) {
+			res = 1;
+		}
+		return res;
+	}
+
+}
---
+++ Tema5/src/parte2/ejercicio2/Futbolista.java
@@ -0,0 +1,65 @@
+package parte2.ejercicio2;
+
+public class Futbolista implements Comparable<Futbolista> {
+
+	private int numCamisa;
+
+	private String nombre;
+
+	private int edad;
+
+	private int numGoles;
+
+	public Futbolista(int numCamisa, String nombre, int edad, int numGoles) {
+
+		if (numCamisa >= 1) {
+			this.numCamisa = numCamisa;
+		}
+
+		if (nombre != null && !nombre.isBlank()) {
+			this.nombre = nombre;
+		}
+
+		if (edad >= 1) {
+			this.edad = edad;
+		}
+
+		if (numGoles >= 0) {
+			this.numGoles = numGoles;
+
+		}
+
+	}
+
+	public int compareTo(Futbolista f) {
+
+		int res = 0;
+
+		if (this.numCamisa == f.numCamisa) {
+			res = this.nombre.compareTo(f.nombre);
+		} else {
+			res = this.numCamisa - f.numCamisa;
+		}
+
+		return res;
+	}
+
+	public boolean equals(Object obj) {
+
+		boolean igu = false;
+
+		Futbolista fut = (Futbolista) obj;
+
+		if (numCamisa == fut.numCamisa && nombre.equals(fut.nombre)) {
+			igu = true;
+		}
+
+		return igu;
+
+	}
+
+	public String toString() {
+		return "Futbolista: " + numCamisa + " - " + nombre + " - " + numGoles + " goles" + " - " + edad + " anyos";
+	}
+
+}
